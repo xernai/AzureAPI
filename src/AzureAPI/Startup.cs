44376735ffc867d41c1652dc512066cff5be9c19
Francisco Javier Montes(UST <U38979@ust-global.com>
2020-01-13 15:15:35 -0600

    removed https redirection

--- src/AzureAPI/Startup.cs
+++ src/AzureAPI/Startup.cs
@@ -36,7 +36,7 @@ namespace AzureAPI
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
 
             app.UseRouting();
 
