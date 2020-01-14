b5233fce45066d741500fd24c3424916fd2148c9
xernai <montcorona@gmail.com>
2020-01-14 14:53:01 -0600

    Adding new weather in controller

--- src/AzureAPI/Controllers/WeatherForecastController.cs
+++ src/AzureAPI/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace AzureAPI.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Template"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
